5450585d66eda7382a76833f632f736971fc1992
renqian805 <84910084+renqian805@users.noreply.github.com>
2022-07-04 23:46:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
 // const fs = require('fs');
-const isDev = require('electron-is-dev');
-const { autoUpdater } = require('electron-updater');
+const isDev = require("electron-is-dev");
+const { autoUpdater } = require("electron-updater");
 
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
 let mainWindow;
 
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 802,
-        height: 824,
-        useContentSize: false,
-        // frame: false,
-        resizable: false,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            enableRemoteModule: false,
-            preload: path.join(__dirname, 'preload.js')
-        },
-    });
+  mainWindow = new BrowserWindow({
+    width: 802,
+    height: 824,
+    useContentSize: false,
+    // frame: false,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    let urlLocation = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './build/index.html')}`;
+  let urlLocation = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "./build/index.html")}`;
 
-    if (isDev) {
-        // mainWindow.webContents.openDevTools();
-    }
+  if (isDev) {
+    // mainWindow.webContents.openDevTools();
+  }
 
-    mainWindow.loadURL(urlLocation);
+  mainWindow.loadURL(urlLocation);
 
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 
-    return mainWindow;
+  return mainWindow;
 };
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-    app.quit();
+app.on("window-all-closed", () => {
+  app.quit();
 });
 
 // let updaterCacheDirName = 'electron-admin-updater'
@@ -53,74 +55,79 @@ app.on('window-all-closed', () => {
 //     provider: 'generic',
 //     url: feedUrl
 // });
-// autoUpdater.setFeedURL({
-//     provider: 'github',
-//     repo: 'electron-demo',
-//     owner: 'myadmin',
-//     private: true,
-//     token: ''
-// });
+autoUpdater.setFeedURL({
+  provider: "github",
+  repo: "electron-demo",
+  owner: "myadmin",
+  private: true,
+  token: "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",
+});
 
 const message = {
-    error: '检查更新出错',
-    checking: '正在检查更新……',
-    updateAva: '检测到新版本，正在下载……',
-    updateNotAva: '现在使用的就是最新版本，不用更新',
+  error: "检查更新出错",
+  checking: "正在检查更新……",
+  updateAva: "检测到新版本，正在下载……",
+  updateNotAva: "现在使用的就是最新版本，不用更新",
 };
 
 if (isDev) {
-    autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml');
+  autoUpdater.updateConfigPath = path.join(__dirname, "dev-app-update.yml");
 }
 autoUpdater.autoDownload = false;
 // autoUpdater.checkForUpdates();
-autoUpdater.on('error', (error) => {
-    // dialog.showErrorBox('Error', err === null ? 'unknown' : err);
-    sendUpdateMessage(`${message.error}:${error}`);
+autoUpdater.on("error", (error) => {
+  // dialog.showErrorBox('Error', err === null ? 'unknown' : err);
+  sendUpdateMessage(`${message.error}:${error}`);
 });
-autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
-    // mainWindow.webContents.send('checking-for-update', 'Checking for update...');
-    sendUpdateMessage(message.checking);
+autoUpdater.on("checking-for-update", () => {
+  console.log("Checking for update...");
+  // mainWindow.webContents.send('checking-for-update', 'Checking for update...');
+  sendUpdateMessage(message.checking);
 });
-autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-        type: 'info',
-        title: '应用有新的更新',
-        message: '发现新版本，是否现在更新？',
-        buttons: ['是', '否']
-    }).then(({ response }) => {
-        if (response === 0) {
-            // mainWindow.webContents.send('updateAvailable', '点击了是');
-            autoUpdater.downloadUpdate();
-            sendUpdateMessage(message.updateAva);
-        }
+autoUpdater.on("update-available", () => {
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "应用有新的更新",
+      message: "发现新版本，是否现在更新？",
+      buttons: ["是", "否"],
+    })
+    .then(({ response }) => {
+      if (response === 0) {
+        // mainWindow.webContents.send('updateAvailable', '点击了是');
+        autoUpdater.downloadUpdate();
+        sendUpdateMessage(message.updateAva);
+      }
     });
 
-    // autoUpdater.downloadUpdate();
-    // sendUpdateMessage(message.updateAva);
+  // autoUpdater.downloadUpdate();
+  // sendUpdateMessage(message.updateAva);
 });
-autoUpdater.on('update-not-available', () => {
-    // dialog.showMessageBox({
-    //     title: '没有新版本',
-    //     message: '当前已经是最新版本'
-    // });
-    // mainWindow.webContents.send('update-not-available', '没有新版本');
-    sendUpdateMessage(message.updateNotAva);
+autoUpdater.on("update-not-available", () => {
+  // dialog.showMessageBox({
+  //     title: '没有新版本',
+  //     message: '当前已经是最新版本'
+  // });
+  // mainWindow.webContents.send('update-not-available', '没有新版本');
+  sendUpdateMessage(message.updateNotAva);
 });
-autoUpdater.on('download-progress', (progress) => {
-    let logMessage = `Download speed: ${progress.bytesPerSecond}`;
-    logMessage = logMessage + ' - Download ' + progress.percent + '%';
-    logMessage = logMessage + ' (' + progress.transferred + '/' + progress.total + ')';
-    console.log(logMessage);
-    mainWindow.webContents.send('downloadProgress', progress);
-    mainWindow.setProgressBar(progress.percent / 100);
+autoUpdater.on("download-progress", (progress) => {
+  let logMessage = `Download speed: ${progress.bytesPerSecond}`;
+  logMessage = logMessage + " - Download " + progress.percent + "%";
+  logMessage =
+    logMessage + " (" + progress.transferred + "/" + progress.total + ")";
+  console.log(logMessage);
+  mainWindow.webContents.send("downloadProgress", progress);
+  mainWindow.setProgressBar(progress.percent / 100);
 });
-autoUpdater.on('update-downloaded', () => {
-    dialog.showMessageBox({
-        title: '安装更新',
-        message: '更新下载完毕，应用将重启并进行安装'
-    }).then(() => {
-        setImmediate(() => autoUpdater.quitAndInstall());
+autoUpdater.on("update-downloaded", () => {
+  dialog
+    .showMessageBox({
+      title: "安装更新",
+      message: "更新下载完毕，应用将重启并进行安装",
+    })
+    .then(() => {
+      setImmediate(() => autoUpdater.quitAndInstall());
     });
 });
 
@@ -135,44 +142,47 @@ autoUpdater.on('update-downloaded', () => {
 //     mainWindow.webContents.send('isUpdateNow')
 // });
 
-ipcMain.on('render-send', (event, arg) => {
-    // console.log('event', event);
-    console.log('arg', arg);
-    dialog.showOpenDialog(mainWindow, {
-        properties: ['openFile', 'openDirectory']
-    }).then(result => {
-        console.log(result.canceled)
-        console.log(result.filePaths)
-        mainWindow.webContents.send('ping', {
-            canceled: result.canceled,
-            filePaths: result.filePaths
-        });
-    }).catch(err => {
-        console.log(err)
+ipcMain.on("render-send", (event, arg) => {
+  // console.log('event', event);
+  console.log("arg", arg);
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ["openFile", "openDirectory"],
+    })
+    .then((result) => {
+      console.log(result.canceled);
+      console.log(result.filePaths);
+      mainWindow.webContents.send("ping", {
+        canceled: result.canceled,
+        filePaths: result.filePaths,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    // dialog.showMessageBox({
-    //     type: 'info',
-    //     title: '应用有新的更新',
-    //     message: '发现新版本，是否现在更新？',
-    //     buttons: ['是', '否']
-    // }).then(({ response }) => {
-    //     if (response === 0) {
-    //         mainWindow.webContents.send('updateAvailable', '点击了是');
-    //         sendUpdateMessage(message.updateAva);
-    //         autoUpdater.downloadUpdate();
-    //     }
-    // });
+  // dialog.showMessageBox({
+  //     type: 'info',
+  //     title: '应用有新的更新',
+  //     message: '发现新版本，是否现在更新？',
+  //     buttons: ['是', '否']
+  // }).then(({ response }) => {
+  //     if (response === 0) {
+  //         mainWindow.webContents.send('updateAvailable', '点击了是');
+  //         sendUpdateMessage(message.updateAva);
+  //         autoUpdater.downloadUpdate();
+  //     }
+  // });
 });
 
-ipcMain.on('checkForUpdate', () => {
-    //放外面的话启动客户端执行自动更新检查
-    autoUpdater.checkForUpdates();
+ipcMain.on("checkForUpdate", () => {
+  //放外面的话启动客户端执行自动更新检查
+  autoUpdater.checkForUpdates();
 });
 
 function sendUpdateMessage(text) {
-    mainWindow.webContents.send('message', text)
+  mainWindow.webContents.send("message", text);
 }
 
-ipcMain.on('checkAppVersion', () => {
-    mainWindow.webContents.send('version', app.getVersion());
+ipcMain.on("checkAppVersion", () => {
+  mainWindow.webContents.send("version", app.getVersion());
 });
